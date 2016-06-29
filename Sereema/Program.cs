90b71ed67320fcb0f174c4feab0584d34e0d1e20
Kevin Michel <kevin.michel@sereema.com>
2016-06-29 18:03:11 +0200

    Allow putting a file to a path different from its filename

--- Sereema/Program.cs
+++ Sereema/Program.cs
@@ -7,16 +7,16 @@ namespace Sereema
     {
         private static void Main(string[] args)
         {
-            if (args.Length == 4)
+            if (args.Length == 5)
             {
                 S3.UploadFile(
-                    Path.GetFileName(args[0]),
+                    args[1].Replace("\\", "/"),
                     File.ReadAllBytes(args[0]),
-                    args[1], args[2], args[3]);
+                    args[2], args[3], args[4]);
             }
             else
             {
-                Console.WriteLine("Usage: Sereema.exe filepath username access_key_id secret_access_key");
+                Console.WriteLine("Usage: Sereema.exe local_filepath remote_filepath username access_key_id secret_access_key");
                 Environment.Exit(-1);
             }
         }
