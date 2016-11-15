029896e7d131fe473072480dc717d83b30e763d9
Kevin Michel <kevin.michel@sereema.com>
2016-11-15 19:32:34 +0100

    Use a credentials text file instead of extra command line arguments
    The credentials file format matches the one exported by the key management interface in webfit.

--- Sereema/Program.cs
+++ Sereema/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace Sereema
@@ -7,18 +8,45 @@ namespace Sereema
     {
         private static void Main(string[] args)
         {
-            if (args.Length == 5)
+            if (args.Length != 3)
             {
-                S3.UploadFile(
-                    args[1].Replace("\\", "/"),
-                    File.ReadAllBytes(args[0]),
-                    args[2], args[3], args[4]);
-            }
-            else
-            {
-                Console.WriteLine("Usage: Sereema.exe local_filepath remote_filepath username access_key_id secret_access_key");
+                Console.WriteLine("Usage: Sereema.exe credentials_filepath local_filepath remote_filepath");
                 Environment.Exit(-1);
             }
+            var credentialsFilepath = args[0];
+            var localFilepath = args[1];
+            // TODO: let remoteFilepath be optional and guessed from localFilepath
+            var remoteFilepath = args[2];
+            string remotePrefix = null;
+            string accessKeyId = null;
+            string secretAccessKey = null;
+            foreach (var line in ReadLines(credentialsFilepath))
+            {
+                ExtractValueMatchingKey(line, "remote_prefix", ref remotePrefix);
+                ExtractValueMatchingKey(line, "access_key_id", ref accessKeyId);
+                ExtractValueMatchingKey(line, "secret_access_key", ref secretAccessKey);
+            }
+            if (remotePrefix == null || accessKeyId == null || secretAccessKey == null)
+            {
+                Console.WriteLine("Error: invalid credentials file");
+                Environment.Exit(-1);
+            }
+            S3.UploadFile(
+                $"{remotePrefix}{remoteFilepath.Replace("\\", "/")}",
+                File.ReadAllBytes(localFilepath), accessKeyId, secretAccessKey);
+        }
+
+        private static IEnumerable<string> ReadLines(string filePath)
+        {
+            using (var streamReader = new StreamReader(filePath))
+                for (var line = streamReader.ReadLine(); line != null; line = streamReader.ReadLine())
+                    yield return line;
+        }
+
+        private static void ExtractValueMatchingKey(string line, string key, ref string value)
+        {
+            if (line.StartsWith($"{key}:"))
+                value = line.Split(':')[1].Trim();
         }
     }
 }
--- Sereema/S3.cs
+++ Sereema/S3.cs
@@ -9,39 +9,40 @@ namespace Sereema
     {
         public const string AwsRegion = "eu-central-1";
         public const string AwsBucket = "sereema-share";
-        public const string AwsFolder = "scada";
         private const string SignedHeader = "host;x-amz-content-sha256;x-amz-date";
 
         // Reference: http://docs.aws.amazon.com/general/latest/gr/sigv4_signing.html
         public static void UploadFile(
-            string fileName, byte[] fileContent,
-            string userName, string accessKeyId, string secretAccessKey)
+            string remotePath, byte[] fileContent,
+            string accessKeyId, string secretAccessKey)
         {
-            var requestPath = string.Format("/{0}/{1}/{2}", userName, AwsFolder, fileName);
-            var hostname = string.Format("{0}.s3-{1}.amazonaws.com", AwsBucket, AwsRegion);
+            if (!remotePath.StartsWith("/"))
+                remotePath = $"/{remotePath}";
+            var hostname = $"{AwsBucket}.s3-{AwsRegion}.amazonaws.com";
             var contentHash = Sha256Hash(fileContent);
             var requestTime = DateTime.UtcNow;
             var requestTimestamp = requestTime.ToString("yyyyMMdd'T'HHmmss'Z'");
             var canonicalRequest = string.Format(
                 "PUT\n{0}\n\nhost:{1}\nx-amz-content-sha256:{2}\nx-amz-date:{3}\n\n{4}\n{2}",
-                Uri.EscapeUriString(requestPath), hostname, contentHash, requestTimestamp, SignedHeader);
-            var scope = string.Format(
-                "{0}/{1}/s3/aws4_request", requestTime.ToString("yyyyMMdd"), AwsRegion);
-            var stringToSign = string.Format(
-                "AWS4-HMAC-SHA256\n{0}\n{1}\n{2}",
-                requestTimestamp, scope,
-                Sha256Hash(Encoding.UTF8.GetBytes(canonicalRequest)));
-            var signingKey = Encoding.UTF8.GetBytes(string.Format("AWS4{0}", secretAccessKey));
+                Uri.EscapeUriString(remotePath), hostname, contentHash, requestTimestamp, SignedHeader);
+            var scope = $"{requestTime.ToString("yyyyMMdd")}/{AwsRegion}/s3/aws4_request";
+            var stringToSign =
+                $"AWS4-HMAC-SHA256\n{requestTimestamp}\n{scope}\n{Sha256Hash(Encoding.UTF8.GetBytes(canonicalRequest))}";
+            var signingKey = Encoding.UTF8.GetBytes($"AWS4{secretAccessKey}");
             foreach (var scopeSegment in scope.Split('/'))
                 signingKey = HmacSha256(signingKey, scopeSegment);
             var signature = Hex(HmacSha256(signingKey, stringToSign));
-            var requestUri = new Uri(string.Format("https://{0}{1}", hostname, requestPath));
-            var webClient = new WebClient();
-            webClient.Headers["authorization"] = string.Format(
-                "AWS4-HMAC-SHA256 Credential={0}/{1},SignedHeaders={2},Signature={3}",
-                accessKeyId, scope, SignedHeader, signature);
-            webClient.Headers["x-amz-content-sha256"] = contentHash;
-            webClient.Headers["x-amz-date"] = requestTimestamp;
+            var requestUri = new Uri(new Uri($"https://{hostname}"), remotePath);
+            var webClient = new WebClient
+            {
+                Headers =
+                {
+                    ["authorization"] =
+                        $"AWS4-HMAC-SHA256 Credential={accessKeyId}/{scope},SignedHeaders={SignedHeader},Signature={signature}",
+                    ["x-amz-content-sha256"] = contentHash,
+                    ["x-amz-date"] = requestTimestamp
+                }
+            };
             webClient.UploadData(requestUri, "PUT", fileContent);
         }
 
